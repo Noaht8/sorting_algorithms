20a157c8894c4d790832ddee083fa679dc3b4d8b
Noah Tsegay <noahtsegay8@gmail.com>
2023-01-24 15:28:47 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -2,6 +2,22 @@
 #define __SORT__
 #include <stdlib.h>
 
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
